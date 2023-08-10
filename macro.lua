08a48602ba666d91c676cafb469acc82797f5032
Dmitrii Panfilov <99917230+DmPanf@users.noreply.github.com>
2023-08-10 03:34:53 +0300

    Create macro.lua

---
+++ macro.lua
@@ -0,0 +1,10 @@
+-- Пример макроса на языке Lua
+function myMacro()
+  print("Привет, я макрос!")
+  for i=1, 5 do
+    print("Я выполняюсь " .. i .. " раз")
+  end
+end
+
+-- Вызов макроса
+myMacro()
